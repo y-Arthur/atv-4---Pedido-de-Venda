373415fffa88c62b4f653531e7c62e827b211af9
Arthur Cardoso <91923216+y-Arthur@users.noreply.github.com>
2022-10-26 19:44:53 -0300

    Update test_ processar_produto.py

--- atv 4 - testes/test_ processar_produto.py
+++ atv 4 - testes/test_ processar_produto.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 from itertools import product
 from Entities.Customer import Customer
 from Entities.Order import Order
@@ -13,41 +13,40 @@ def test_processa_produto():
     custumer1 = Customer(1, "Arthur")
     repositoryCustumer = CustomerRepository()
     repositoryCustumer.append_customer(custumer1)
-    
+
     product1 = Product(3, "Rice", 20, 5)
     repositoryProduct = ProductRepository()
     repositoryProduct.append_product(product1)
-    
-    ordem = Order(1, custumer1)
+
+    ordem = Order(1, custumer1, datetime.today)
     ordem_produto1 = OrderProduct()
-    ordem_produto1.add_product(product1,10)
+    ordem_produto1.add_product(product1, 2)
     ordem.add_order_product(ordem_produto1)
-    
-    #Act
+
+    # Act
     ordem.update_total_price()
-    
-    #Assert
-    assert product1.get_stock() == 10
-    
+
+    # Assert
+    assert product1.check_stock(2) == True
+
+
 def test_integrado_baixar_estoque():
-    #Arrange
-    product = Product(3,"Rice",10,2)
-   
-    #Act
-    product.down_stock(5)
-    
-    #Assert
-    product.get_stock() == 15
+    # Arrange
+    product = Product(3, "Rice", 50, 20)
+
+    # Act
+    product.down_stock(20) 
+
+    # Assert
+    assert product.stock == 0
+
 
 def test_baixar_estoque():
-    #Arrange
-    produto = Product(1,"Salt",3.77,10)
+    # Arrange
+    produto = Product(1, "Salt", 3.77, 10)
 
-    #ACT
+    # ACT
     produto.down_stock(7)
 
-    #Assert
+    # Assert
     assert produto.stock == 3
-
-    
-
